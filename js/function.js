a8a2169d78c43448eadff481e5b82a64dd326379
Sinapsis Soft <sinapsis.soft.developer@gmail.com>
2018-03-26 23:56:11 -0500

    new changes

--- js/function.js
+++ js/function.js
@@ -13,37 +13,77 @@ $('#ModalInformation').on('show.bs.modal', function (event) {
   /*modal.find('.modal-body input').val(recipient)*/
   //alert(titles[recipient-1]);
 	
-  $('#text-modal-container').html(textContainer[recipient]);	
+ // $('#text-modal-container').html(textContainer[recipient]);	
 
  
-  
+
+  $('.modal-body').html(LoadData(recipient));	
  
-  loadData(0);
+
 });
-function loadData(section){
+
+
+function LoadData(section){
+
   var linkVideo="";// is link video youtube 
   var linkImg="";// is link video youtube
   var linkLocation="";// is link video youtube
   var containerModal='<div class="row mx-auto" >';//structor general
-  var containerModalVideo='<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"><iframe style="width:100%; height:300px" src="'+linkVideo+'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';//structor video
+  var containerModalVideo="";
   var containerModalText='<div id="text-modal-container" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">';
   var containerModalImgLink='<div><a href="'+linkLocation+'"><img class="img-responsive" src="'+ linkImg+'"></a></div>';
+  var textContainer="";
+  var dataContainer=[];
+
+
+  var myObj={"blocks":[
+    {"title":"Transformación Digital","container":[
+      {"type":"video","title":"Transformación Digital","cont":"https://www.youtube.com/embed/6ZeMEae6pGs"},
+      {"type":"text","title":"","cont":"<p class='text-justify'>El impacto de las tecnologías emergentes en la transformación digital de las empresas no ha hecho más que comenzar y dará lugar a un antes y un después. La transformación digital en todas las empresas se dará en todos los sectores y supondrá una aportación importante al crecimiento del PIB y al empleo a nivel general. La movilidad, la computación en la nube y Big Data ya comienzan a ser una fuente importante de negocio, ante la que se están ya tomado posiciones. Internet de todas las cosas (IoE) es probablemente la tecnología que aportará la mayor parte de la transformación digital, convirtiéndose en la tecnología de uso general donde se conectará cualquier cosa y personas y además hará posible el desarrollo de las Smart Cities y Factory 4.0. <br>Obviamente las empresas que ya nacen “nativas digitales” no se tienen que transformar. Las que necesitan transformarse son las empresas tradicionales de servicios e industriales, de todos los sectores, porque es un requisito fundamental para sobrevivir. La pregunta es:<br></p><p class='text-center'><b>¿Por dónde empezar?</b></p>"}
+        ]}
+    ]}
+
+  var objectCont=myObj.blocks[section].container;
+  dataContainer.push(containerModal);
+
+  for(i in objectCont){
+    
+      if(objectCont[i].type=="video"){
+        linkVideo=myObj.blocks[0].container[i].cont;
+          containerModalVideo='<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"><iframe style="width:100%; height:300px" src="'+linkVideo+'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
+          dataContainer.push(containerModalVideo);
+      }else if(objectCont[i].type=="text"){
+        containerModalText+=objectCont[i].cont+"</div>";
+          dataContainer.push(containerModalText);
+      }
+      
+  }
   
+ 
+
+  dataContainer.push("</div>");
+ 
+  for(var j=0;j<dataContainer.length;j++){
+
+    textContainer+=dataContainer[j];
+  }
+
+  return textContainer;
+
+};
+function Data(section){
+
+  var linksVideo={"video":[
+    {"section":0,"link":"https://www.youtube.com/embed/6ZeMEae6pGs"},
+    {"section":1,"link":"https://www.youtube.com/embed/xMPWnAj6fx8"},
+    {"section":1,"link":"https://www.youtube.com/embed/UaoQ7mq7QTM"},
+    {"section":1,"link":"https://www.youtube.com/watch?v=Z6eB3eUEieU"},
+    {"section":1,"link":"https://drive.google.com/file/d/0B6m88QgUmsMOSmtzM3N0cXA2MHM/view"},
+    {"section":1,"link":"https://www.youtube.com/embed/hHuO8MEqKDY"},
+    {"section":1,"link":"https://www.youtube.com/embed/LoVt5lsmC6M"}
+]};
+
+
 }
 
-<div class="row mx-auto" >  
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <iframe style="width:100%; height:300px" src="https://www.youtube.com/embed/6ZeMEae6pGs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-          </div>
-          <div id="text-modal-container" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p> El impacto de las tecnologías emergentes en la transformación digital de las empresas no ha hecho más que comenzar y dará lugar a un antes y un después. La transformación digital en todas las empresas se dará en todos los sectores y supondrá una aportación importante al crecimiento del PIB y al empleo a nivel general.
-            <br>
-           <br>
-            La movilidad, la computación en la nube y Big Data ya comienzan a ser una fuente importante de negocio, ante la que se están ya tomado posiciones. Internet de todas las cosas (IoE) es probablemente la tecnología que aportará la mayor parte de la transformación digital, convirtiéndose en la tecnología de uso general donde se conectará cualquier cosa y personas y además hará posible el desarrollo de las Smart Cities y Factory 4.0</p>
-            <br>
-            Obviamente las empresas que ya nacen “nativas digitales” no se tienen que transformar. Las que necesitan transformarse son las empresas tradicionales de servicios e industriales, de todos los sectores, porque es un requisito fundamental para sobrevivir. La pregunta es: ¿Por dónde empezar? </p>
-          </div>
-          <div> 
-            <a href="https://cloud.google.com/compute/?hl=es"><img class="img-responsive" src="img/news/imgLinks/link_xertica.fw.png"></a>
-          </div>
-</div>
+
